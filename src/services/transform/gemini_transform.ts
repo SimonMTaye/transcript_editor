861b56aa506605ace74817d49dacc8e24c9d3e8e
Simon Taye <mulat.simon@gmail.com>
2025-05-12 23:44:32 -0400

    Create factories for interface implementations
    - Using factory lets us avoid top-level constants
    - Easier to create implementations that run on the server and are wrapped

--- src/services/transform/gemini_transform.ts
+++ src/services/transform/gemini_transform.ts
@@ -7,10 +7,10 @@ if (!apiKey) {
   throw new Error("GEMINI_API_KEY environment variable not set.");
 }
 
-const genAI = new GoogleGenAI({apiKey: apiKey});
-const model = 'gemini-2.5-flash-preview-04-17';
-const segmentResponseType= {
-  responseMimeType: 'application/json',
+const genAI = new GoogleGenAI({ apiKey: apiKey });
+const defaultModel = "gemini-2.5-flash-preview-04-17";
+const segmentResponseType = {
+  responseMimeType: "application/json",
   responseSchema: {
     type: Type.OBJECT,
     required: ["segments"],
@@ -42,43 +42,52 @@ const segmentResponseType= {
 
 // Convert the segments to string [start - end ?speaker]: text \n
 const flatten = (segments: TranscriptSegment[]) => {
-  return segments.map((segment) => {
-    return `${segment.start} - ${segment.end} ${segment.speaker ? `?${segment.speaker}` : ''}: ${segment.text}\n`;
-  }).join('');
+  return segments
+    .map((segment) => {
+      return `${segment.start} - ${segment.end} ${
+        segment.speaker ? `?${segment.speaker}` : ""
+      }: ${segment.text}\n`;
+    })
+    .join("");
 };
 
 const systemInstruction = `You are an excellent content editor and transcriptor. Please refine this interview transcript to improve readability and make it appropriate for a written post.
 Remove speech artifacts (ums, ahs, repeated words), fix obvious grammatical errors and delete pleasantries with no other content.
 Remove redundant content, ensure natural flow, shorten where appropriate and remove unnecessary connective words.
 Maintain all important information, and if you decide to change wording then make sure to preserve the original intention.
-Try to reduce the transcript to 1500-2500 words and make each segment between 10 and 45s where appropriate.`;
+Try to reduce the transcript to 1500-2500 words. 
+If speaker names are present, please preserve them; if they are not present, then assign generic Speaker 1, Speaker 2, etc. to the segments using context to know when the speaker changes.`;
 
-export const refine = async (segments: TranscriptSegment[]) => {
-  const response = await genAI.models.generateContent({
-    model, 
-    contents: [
-      {
-        role: 'user',
-        parts: [
+export const refineFactory = async (googleAI: GoogleGenAI, model: string) => {
+  return async (segments: TranscriptSegment[]) => {
+    const response = await googleAI.models.generateContent({
+      model,
+      contents: [
         {
-          text: flatten(segments)
-        }
-      ]
-      }
-    ],
-    config: {
-      systemInstruction,
-      ...segmentResponseType,
-    }
-  });
-  // Parse the response
-  const parsedResponse = JSON.parse(response.text!);
-  return parsedResponse.segments.map((segment: any) => {
-    return {
-      start: segment.start,
-      end: segment.end,
-      speaker: segment.speaker,
-      text: segment.text,
-    };
-  }) as TranscriptSegment[];
+          role: "user",
+          parts: [
+            {
+              text: flatten(segments),
+            },
+          ],
+        },
+      ],
+      config: {
+        systemInstruction,
+        ...segmentResponseType,
+      },
+    });
+    // Parse the response
+    const parsedResponse = JSON.parse(response.text!);
+    return parsedResponse.segments.map((segment: any) => {
+      return {
+        start: segment.start,
+        end: segment.end,
+        speaker: segment.speaker,
+        text: segment.text,
+      };
+    }) as TranscriptSegment[];
+  };
 };
+
+export const refine = await refineFactory(genAI, defaultModel);
