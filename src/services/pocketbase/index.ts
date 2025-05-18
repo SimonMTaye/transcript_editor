1834d136c562c58da6ec84984684b6e4c6489a97
Simon Taye <mulat.simon@gmail.com>
2025-05-18 12:01:52 -0400

    Use correct env name for pocketbase backend

--- src/services/pocketbase/index.ts
+++ src/services/pocketbase/index.ts
@@ -2,7 +2,7 @@ import PocketBase from "pocketbase";
 import { pbDB } from "./pocketbase_db";
 import { pbFileStore } from "./pocketbase_store";
 
-const PB_URL = import.meta.env.VITE_PB_URL;
+const PB_URL = import.meta.env.VITE_POCKETB_URL;
 console.log("PocketBase URL:", PB_URL);
 export const pb = new PocketBase(PB_URL);
 
