8033acceb9b1272893e171d387acbe9f6e2401b9
Simon Taye <mulat.simon@gmail.com>
2025-05-17 23:50:53 -0400

    Remove worker-related stuff from vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@src": path.resolve(__dirname, "./src"),
       "@shared": path.resolve(__dirname, "./shared"),
-      "@worker": path.resolve(__dirname, "./worker"),
     },
   },
 });
